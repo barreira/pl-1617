2b9e25a7385584f052c7a7929218e2c66bdcc9e4
João Pires Barreira <jpiresbarreira@gmail.com>
2017-04-23 22:51:48 +0100

    Adicionado exemplo 2

---
+++ TP1b/6/exemplo2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+char* FraseExemplo()
+{
+	char BUF[10000];
+	int j = 0;
+	j += sprintf(BUF + j, " Funcao sem argumentos \n");
+	return strdup(BUF);
+}
+
+
+int main(){
+	printf("%s\n", FraseExemplo());
+}
